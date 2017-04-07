3aec1d952f5be1d7c70f751ba8802fb2818baff5
Wei Yen <wlee@mathspace.co>
2017-04-07 23:34:30 +1000

    Implement ObjectType

---
+++ polygraph/types/object_type.py
@@ -0,0 +1,29 @@
+from collections import OrderedDict
+
+from graphql.type.definition import GraphQLObjectType
+from marshmallow import Schema, SchemaOpts
+
+
+class ObjectTypeOpts(SchemaOpts):
+    def __init__(self, meta, **kwargs):
+        SchemaOpts.__init__(self, meta, **kwargs)
+        self.name = getattr(meta, 'name', None)
+        self.description = getattr(meta, 'name', None)
+
+
+class ObjectType(Schema):
+    OPTIONS_CLASS = ObjectTypeOpts
+
+    def __init__(self, only=(), exclude=(), prefix='', strict=None,
+                 many=False, context=None, load_only=(), dump_only=(),
+                 partial=False):
+        super().__init__(only, exclude, prefix, strict,
+                         many, context, load_only, dump_only, partial)
+        self._name = self.opts.name or self.__class__.__name__
+        self._description = self.opts.description or self.__doc__
+
+    def build_definition(self):
+        field_map = OrderedDict()
+        for fieldname, field in self.fields.items():
+            field_map[fieldname] = field.build_definition()
+        return GraphQLObjectType(name=self._name, fields=field_map)
