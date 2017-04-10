8373c005cbf8ebc4069faf5291bb126db2cbb20f
Wei Yen <wlee@mathspace.co>
2017-04-10 21:44:45 +1000

    Add additional tests around scalars

--- polygraph/types/tests/test_scalars.py
+++ polygraph/types/tests/test_scalars.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
-from polygraph.types.scalar import Int
+
+from polygraph.types.scalar import Boolean, Float, Int, String
 
 
 class IntTest(TestCase):
@@ -13,3 +14,26 @@ class IntTest(TestCase):
         self.assertIsInstance(y, int)
         self.assertIsInstance(y, Int)
         self.assertEqual(y, 506)
+
+
+class StringTest(TestCase):
+    def test_class_types(self):
+        x = String("What is this?")
+        self.assertIsInstance(x, str)
+        self.assertIsInstance(x, String)
+        self.assertEqual(x, "What is this?")
+
+
+class FloatTest(TestCase):
+    def test_class_types(self):
+        x = Float(2.84)
+        self.assertIsInstance(x, float)
+        self.assertIsInstance(x, Float)
+        self.assertEqual(x + 1, 3.84)
+
+
+class BooleanTest(TestCase):
+    def test_class_types(self):
+        self.assertTrue(Boolean(True))
+        self.assertFalse(Boolean(False))
+        self.assertEqual(Boolean(True)._type.name, "Boolean")
