77c18437a6ed54209ca4c5b7e5de488ac9447bd1
Wei Yen <wlee@mathspace.co>
2017-04-14 19:14:41 +1000

    Implement Field object

---
+++ polygraph/types/field.py
@@ -0,0 +1,8 @@
+class Field:
+    def __init__(self, name, return_type, description=None, arg_types=None, deprecation_reason=None):
+        self.name = name
+        self.return_type = return_type
+        self.description = description
+        self.arg_types = arg_types
+        self.deprecation_reason = deprecation_reason
+        self.is_deprecated = bool(deprecation_reason)
