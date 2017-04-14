f5f3ba1e6e9cb471fdbb7f356cdb6524863fc9b2
Wei Yen <wlee@mathspace.co>
2017-04-14 19:15:25 +1000

    Rewrote test_object_type to check on field types

--- polygraph/types/tests/test_object_type.py
+++ polygraph/types/tests/test_object_type.py
@@ -1,18 +1,10 @@
-from collections import OrderedDict
-from pprint import pformat
 from unittest import TestCase
 
-from graphql.type.scalars import GraphQLString
-
-from polygraph.types.definitions import (
-    PolygraphField,
-    PolygraphNonNull,
-    PolygraphObjectType,
-)
-from polygraph.types.decorators import field
-from polygraph.types.scalar import String
-from polygraph.types.object_type import ObjectType
+from polygraph.exceptions import PolygraphValueError
 from polygraph.types.basic_type import NonNull
+from polygraph.types.decorators import field
+from polygraph.types.object_type import ObjectType
+from polygraph.types.scalar import Int, String
 
 
 class HelloWorldObject(ObjectType):
@@ -20,36 +12,32 @@ class HelloWorldObject(ObjectType):
     This is a test object
     """
     @field()
-    def first(self) -> String:
-        """First violin"""
-        return "Stradivarius"
+    def greet_world(self) -> String:
+        """Generic message to the world"""
+        return "Hello world!"
 
     @field()
-    def second(self) -> NonNull(String):
-        """Second fiddle"""
-        pass
+    def greet_you(self, your_name: NonNull(String)) -> NonNull(String):
+        """Greeting by name"""
+        return "Hello, {}!".format(your_name)
 
-    @field(deprecation_reason="Third is dead")
-    def third(self) -> NonNull(String):
-        pass
+    @field(deprecation_reason="Wrong return type")
+    def bad_resolver(self) -> Int:
+        return "three"
 
 
-# class ObjectTypeTest(TestCase):
-#     def test_simple_object_type(self):
-#         hello_world = HelloWorldObject()
-#         expected = PolygraphObjectType(
-#             name="HelloWorldObject",
-#             description="This is a test object",
-#             fields=OrderedDict({
-#                 "first": PolygraphField(GraphQLString, None, None, None, "First violin"),
-#                 "second": PolygraphField(
-#                     PolygraphNonNull(GraphQLString), None, None, None, "Second fiddle"),
-#                 "third": PolygraphField(
-#                     PolygraphNonNull(GraphQLString), None, None, "Third is dead", None),
-#             })
-#         )
-#         actual = hello_world.build_definition()
-#         self.assertEqual(
-#             expected, actual,
-#             "\n\n{}\n\n!=\n\n{}".format(pformat(expected, indent=4), pformat(actual, indent=4))
-#         )
+class SimpleObjectTypeTest(TestCase):
+    def test_bare_resolver(self):
+        hello_world = HelloWorldObject()
+        self.assertEqual(hello_world.greet_world(), String("Hello world!"))
+        self.assertEqual(hello_world.greet_you(your_name="Bill"), String("Hello, Bill!"))
+
+    def test_resolver_argument(self):
+        hello_world = HelloWorldObject()
+        with self.assertRaises(PolygraphValueError):
+            hello_world.greet_you(your_name=None)
+
+    def test_bad_resolver(self):
+        hello_world = HelloWorldObject()
+        with self.assertRaises(PolygraphValueError):
+            hello_world.bad_resolver()
