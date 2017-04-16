4963b702eaf99feb1e73399b9580a9650ae50302
Wei Yen <wlee@mathspace.co>
2017-04-16 20:54:02 +1000

    Fix enum types to behave like other types

--- polygraph/types/tests/test_enum.py
+++ polygraph/types/tests/test_enum.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 
+from polygraph.exceptions import PolygraphValueError
 from polygraph.types.enum import EnumType
 
 
@@ -12,14 +13,19 @@ class Colours(EnumType):
 class EnumTest(TestCase):
 
     def test_simple_enum(self):
-        red = Colours.RED.value
+        red = Colours.RED
         self.assertEqual(red.name, "RED")
         self.assertEqual(red.description, "The colour of fury")
 
-        green = Colours.GREEN.value
+        green = Colours.GREEN
         self.assertEqual(green.name, "GREEN")
         self.assertEqual(green.description, "The colour of envy")
 
-        blue = Colours.BLUE.value
+        blue = Colours.BLUE
         self.assertEqual(blue.name, "BLUE")
         self.assertEqual(blue.description, "The colour of sloth")
+
+    def test_enum_value(self):
+        self.assertEqual(Colours(Colours.RED), Colours.RED)
+        with self.assertRaises(PolygraphValueError):
+            Colours("RED")
