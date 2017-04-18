12f51ce0d5e6c67e15ba3d60c21523a28d29b964
Wei Yen <wlee@mathspace.co>
2017-04-19 00:12:42 +1000

    Add tests for the type map builder

---
+++ polygraph/types/tests/test_type_map.py
@@ -0,0 +1,41 @@
+from unittest import TestCase
+
+from polygraph.types.decorators import field
+from polygraph.types.object_type import ObjectType
+from polygraph.types.scalar import ID, Boolean, Float, Int, String
+from polygraph.types.schema import Schema
+from polygraph.types.type_builder import List, NonNull, Union
+
+
+class Person(ObjectType):
+    @field()
+    def name(self) -> NonNull(String):
+        pass
+
+    @field()
+    def age(year: Int) -> String:
+        pass
+
+
+class Animal(ObjectType):
+    @field()
+    def can_walk(self) -> Boolean:
+        pass
+
+
+class Query(ObjectType):
+    @field()
+    def characters(self) -> List(Union(Animal, Person)):
+        pass
+
+
+class TypeMapTest(TestCase):
+    def test_type_map_builder(self):
+        schema = Schema(query=Query, additional_types=[ID])
+        type_map = schema.type_map
+        self.assertEqual(type_map["Animal"], Animal)
+        self.assertEqual(type_map["Person"], Person)
+        self.assertEqual(type_map["Animal|Person"], Union(Animal, Person))
+        self.assertEqual(type_map["Boolean"], Boolean)
+        self.assertEqual(type_map["ID"], ID)
+        self.assertNotIn(Float, type_map.values())  # Float was not defined anywhere
