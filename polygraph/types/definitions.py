2444b6069a22503b1c1d630e2a9cc8f1f651ff20
Wei Yen <wlee@mathspace.co>
2017-04-18 22:02:18 +1000

    Move EnumValue to definitions

--- polygraph/types/definitions.py
+++ polygraph/types/definitions.py
@@ -29,6 +29,20 @@ Field = namedtuple(
 )
 
 
+class EnumValue:
+    __slots__ = ["name", "description", "is_deprecated", "deprecation_reason", "parent"]
+
+    def __init__(self, name, parent, description=None, deprecation_reason=None):
+        self.name = name
+        self.description = description
+        self.is_deprecated = bool(deprecation_reason)
+        self.deprecation_reason = deprecation_reason
+        self.parent = parent
+
+    def __repr__(self):
+        return "EnumValue('{}')".format(self.name)
+
+
 TypeDefinition = namedtuple(
     "TypeDefinition",
     [
