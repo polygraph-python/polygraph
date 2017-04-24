e4ff25bb5f60598f74f1a5248f67d888a10b71da
Wei Yen <wlee@mathspace.co>
2017-04-25 00:18:47 +1000

    Implement `get_type_class`
    Automatically resolves lazy types

---
+++ polygraph/types/tests/test_api.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+from polygraph.types.lazy_type import LazyType
+from polygraph.types.scalar import Int
+from polygraph.types.api import get_type_class
+from polygraph.types.tests.helper import Person
+
+
+class APITest(TestCase):
+    def test_get_type_class(self):
+        self.assertEqual(
+            get_type_class(LazyType("Person", "polygraph.types.tests.helper")),
+            Person,
+        )
+        self.assertEqual(
+            get_type_class(Int),
+            Int,
+        )
