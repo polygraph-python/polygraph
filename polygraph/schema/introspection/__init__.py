04cb8c662ca860fd826c45eaf7705a8cb905c708
Wei Yen <wlee@mathspace.co>
2017-05-17 22:07:17 +1000

    Initial commit for schema introspection

---
+++ polygraph/schema/introspection/__init__.py
@@ -0,0 +1,8 @@
+from .models import TypeKind
+from .type_definition import type_definition
+
+
+__all__ = (
+    "type_definition",
+    "TypeKind",
+)
---
+++ polygraph/schema/introspection/models.py
@@ -0,0 +1,32 @@
+from enum import Enum
+from collections import namedtuple
+
+
+TypeDefinition = namedtuple(
+    "TypeDefinition",
+    [
+        "kind",
+        "name",
+        "description",
+        "possible_types",
+        "fields",
+        "interfaces",
+        "enum_values",
+        "input_fields",
+        "of_type",
+    ]
+)
+
+
+class TypeKind(Enum):
+    SCALAR = "Represents scalar types such as Int, String, and Boolean. Scalars cannot have fields."
+    OBJECT = "Object types represent concrete instantiations of sets of fields. "
+    UNION = "Unions are an abstract type where no common fields are declared."
+    INTERFACE = "Interfaces are an abstract type where there are common fields declared."
+    ENUM = "Enums are special scalars that can only have a defined set of values."
+    INPUT_OBJECT = "Input objects are composite types used as inputs into queries defined as a "\
+                   "list of named input values."
+    LIST = "Lists represent sequences of values in GraphQL."
+    NON_NULL = "A Non‐null type is a type modifier: it wraps another type instance in the "\
+               "ofType field. Non‐null types do not allow null as a response, and indicate "\
+               "required inputs for arguments and input object fields."
